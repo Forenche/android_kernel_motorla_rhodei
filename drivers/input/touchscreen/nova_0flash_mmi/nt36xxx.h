1df315558259a105c4e15d535fe06bc687875c0d
Forenche <prahul2003@gmail.com>
2022-10-20 15:50:58 +0530

    nova_0flash_mmi: Disable logging and self-testing
    Signed-off-by: Forenche <prahul2003@gmail.com>

--- drivers/input/touchscreen/nova_0flash_mmi/nt36xxx.h
+++ drivers/input/touchscreen/nova_0flash_mmi/nt36xxx.h
@@ -120,7 +120,7 @@ extern const uint16_t touch_key_array[TOUCH_KEY_NUM];
 //---Customerized func.---
 #define NVT_TOUCH_PROC 1
 #define NVT_TOUCH_EXT_PROC 1
-#define NVT_TOUCH_MP 1
+#define NVT_TOUCH_MP 0
 #define MT_PROTOCOL_B 1
 #if defined (NVT_SENSOR_EN) || defined (CONFIG_INPUT_TOUCHSCREEN_MMI)
 #define WAKEUP_GESTURE 1
